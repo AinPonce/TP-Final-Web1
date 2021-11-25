fe455ce7f09dda52835a06a24758b872629aaa44
Ain Ponce <ponce.ain@gmail.com>
2021-11-25 10:11:27 -0300

    iniciamos repo

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const provincia = [
+    {id: 1, name: "Buenos Aires"},
+    {id: 2, name:"Misiones"},
+    {id: 3, name:"San Juan"},
+    {id: 4, name:"Salta"
+    }];
+
+const establecimiento = [
+    {id: 1, name:"San Justo"},
+    {id: 1, name:"Casanova"},
+    {id: 1, name:"Ramos Mejia"},
+    {id: 1, name:"Belgrano"},
+    {id: 2, name:"Posadas"},
+    {id: 2, name:"Apostoles"},
+    {id: 2, name:"Eldorado"},
+    {id: 2, name:"Montecarlo"},
+    {id: 3, name:"Rawson"},
+    {id: 3, name:"Rivadavia"},
+    {id: 3, name:"Chimbas"},
+    {id: 3, name:"Gaucete"},
+    {id: 4, name:"La Viña"},
+    {id: 4, name:"Iruya"},
+    {id: 4, name:"Cafayae"},
+    {id: 4, name:"Molinos"
+}];
